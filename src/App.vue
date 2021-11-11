362f37bb32984393690fc9b507f7f81d2bb09cba
masfuulaji <masfuulaji@gmail.com>
2021-11-11 20:35:18 +0700

    basic modal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="book" @close="toggleModal"/>
+  </div>
+  <button @click="toggleModal">Just Click it</button>
 </template>
 
 <script>
+import Modal from "./components/Modal.vue";
+
 export default {
   name: "App",
+  components: {
+    Modal,
+  },
   data() {
     return {
       title: "My App D:",
+      header: "Who Am I",
+      text: "industrial society and its future",
+      showModal: false,
     };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
   },
 };
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'book' }">
+      <h3>{{ header }}</h3>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h3 {
+  color: white;
+}
+</style>
