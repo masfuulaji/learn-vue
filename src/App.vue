f82157617167289bf8f05196b0f105b598cf2a2c
masfuulaji <masfuulaji@gmail.com>
2021-11-12 20:58:40 +0700

    boomer game

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <h1>{{ title }}</h1>
-  <div v-if="showModal">
-    <Modal theme="book" @close="toggleModal">
-      <template v-slot:type>
-        <span>Manifesto</span>
-      </template>
-      <h3>Who Am I</h3>
-      <p>Industrial society and its future</p>
-    </Modal>
-  </div>
-  <button @click="toggleModal">Just Click it</button>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Click it</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResult" :score="score" />
 </template>
 
 <script>
-import Modal from "./components/Modal.vue";
-
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 export default {
   name: "App",
-  components: {
-    Modal,
-  },
+  components: { Block, Result },
   data() {
     return {
-      title: "My App D:",
-      showModal: false,
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
     };
   },
   methods: {
-    toggleModal() {
-      this.showModal = !this.showModal;
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame(val) {
+      this.score = val;
+      this.isPlaying = false;
+      this.showResult = true;
     },
   },
 };
@@ -40,7 +40,23 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Not Human";
+    } else if (this.score < 500) {
+      this.rank = "Boomer";
+    } else {
+      this.rank = "Monkey Brain";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
