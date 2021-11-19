22cfb6b301eab7d964137aedf1d884ab6794bc1b
masfuulaji <masfuulaji@gmail.com>
2021-11-20 00:09:51 +0700

    commit this

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">Home</div>
+  <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+  <button @click="clickThis">Just Click It</button>
+  <input type="text" v-model="name" />
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    const name = ref("budi");
+    const age = ref(20);
+
+    // const p = ref(null);
+
+    const clickThis = () => {
+      name.value = "dimas";
+      age.value = "30";
+    };
+
+    return { name, age, clickThis };
+  },
+};
 </script>
